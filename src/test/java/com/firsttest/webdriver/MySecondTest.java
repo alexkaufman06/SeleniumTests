f4f82c359e34bbdeccfb5d825b2a2fda60c04d66
alexkaufman06 <alexkaufman06@gmail.com>
2018-04-08 15:02:47 -0700

    Add second test

--- src/test/java/com/firsttest/webdriver/MySecondTest.java
+++ src/test/java/com/firsttest/webdriver/MySecondTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MyFirstTest {
+public class MySecondTest {
     @Test
     public void startWebDriver() {
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://www.facebook.com/");
+        driver.navigate().to("https://www.reddit.com/");
 
         //Step 3- Assertion: Check its title is correct
         //assertEquals method Parameters: Message, Expected Value, Actual Value
-        Assert.assertEquals("Title check failed!", "Facebook - Log In or Sign Up", driver.getTitle());
+        Assert.assertEquals("Title check failed!", "reddit: the front page of the internet", driver.getTitle());
 
         //Step 4- Close Driver
         driver.close();
