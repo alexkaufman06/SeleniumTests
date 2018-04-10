4f6f0c36559f6725a517fc865d1d24469ad0e269
alexkaufman06 <alexkaufman06@gmail.com>
2018-04-10 15:34:34 -0700

    Add third test and refactor

--- src/test/java/com/firsttest/webdriver/MyFirstTest.java
+++ src/test/java/com/firsttest/webdriver/MyFirstTest.java
@@ -9,17 +9,9 @@ public class MyFirstTest {
     @Test
     public void startWebDriver() {
         WebDriver driver = new ChromeDriver();
-
         driver.navigate().to("https://www.facebook.com/");
-
-        //Step 3- Assertion: Check its title is correct
-        //assertEquals method Parameters: Message, Expected Value, Actual Value
         Assert.assertEquals("Title check failed!", "Facebook - Log In or Sign Up", driver.getTitle());
-
-        //Step 4- Close Driver
         driver.close();
-
-        //Step 5- Quit Driver
         driver.quit();
     }
 }
--- src/test/java/com/firsttest/webdriver/MyThirdTest.java
+++ src/test/java/com/firsttest/webdriver/MyThirdTest.java
@@ -3,12 +3,12 @@ package com.firsttest.webdriver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class MySecondTest {
+public class MyThirdTest {
     @Test
     public void startWebDriver() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new InternetExplorerDriver();
         driver.navigate().to("https://www.reddit.com/");
         Assert.assertEquals("Title check failed!", "reddit: the front page of the internet", driver.getTitle());
         driver.close();
