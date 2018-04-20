69d6ed5fa5c6f6e14bd3ceb381e849d1f85d000d
alexkaufman06 <alexkaufman06@gmail.com>
2018-04-20 13:14:47 -0700

    Update tests

--- src/test/java/com/firsttest/webdriver/ParallelTest1.java
+++ src/test/java/com/firsttest/webdriver/ParallelTest1.java
@@ -1,6 +1,10 @@
 package com.firsttest.webdriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 
@@ -8,24 +12,39 @@ public class ParallelTest1 {
     @Test
     public void testChrome1() throws MalformedURLException {
         WebDriver driver = new DriverManager().getDriver("chrome");
-        driver.navigate().to("https://www.facebook.com/");
-        driver.manage().window().maximize();
+        driver.navigate().to("https://www.fisherinvestments.com/en-us/campaigns/99rt/la");
+        WebElement Button = driver.findElement(By.className("btn-Landing2"));
+        Button.click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement firstNameInput;
+        firstNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("DesktopForm_TXT_FIRST_NAME")));
+        firstNameInput.sendKeys("test");
         driver.close();
     }
 
     @Test
     public void testChrome2() throws MalformedURLException {
         WebDriver driver = new DriverManager().getDriver("chrome");
-        driver.navigate().to("https://www.reddit.com/");
-        driver.manage().window().maximize();
+        driver.navigate().to("https://www.fisherinvestments.com/en-us/campaigns/ai/lb");
+        WebElement Button = driver.findElement(By.className("btn-Landing2"));
+        Button.click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement firstNameInput;
+        firstNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("DesktopForm_TXT_FIRST_NAME")));
+        firstNameInput.sendKeys("test");
         driver.close();
     }
 
     @Test
     public void testFirefox1() throws MalformedURLException {
         WebDriver driver = new DriverManager().getDriver("firefox");
-        driver.navigate().to("https://www.amazon.com");
-        driver.manage().window().maximize();
+        driver.navigate().to("https://www.fisherinvestments.com/en-us/campaigns/air/lx");
+        WebElement Button = driver.findElement(By.className("btn-Landing2"));
+        Button.click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement firstNameInput;
+        firstNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("DesktopForm_TXT_FIRST_NAME")));
+        firstNameInput.sendKeys("test");
         driver.close();
     }
 }
