557f7dcd2cd6b127f0be518967809c0e1590d714
alexkaufman06 <alexkaufman06@gmail.com>
2018-04-24 15:26:52 -0700

    Add array of urls

--- src/test/java/com/firsttest/webdriver/ParallelTest2.java
+++ src/test/java/com/firsttest/webdriver/ParallelTest2.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import java.net.MalformedURLException;
 
 public class ParallelTest2 extends DriverManager {
+    public String[] urls = {"https://www.reddit.com","https://www.google.com","https://www.npr.org"};
+    int size = urls.length;
+
     @Test
     public void testChrome3() throws MalformedURLException {
         WebDriver driver = new DriverManager().getDriver("chrome");
